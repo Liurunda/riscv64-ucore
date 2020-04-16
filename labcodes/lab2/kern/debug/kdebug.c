7dfe56c24944f0669136a5712c6914b754f84a44
曹鼎原 <cdy17@mails.tsinghua.edu.cn>
2020-04-16 23:12:59 +0800

    Change to 64bit output.

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -10,10 +10,10 @@
 void print_kerninfo(void) {
     extern char etext[], edata[], end[], kern_init[];
     cprintf("Special kernel symbols:\n");
-    cprintf("  entry  0x%08x (virtual)\n", kern_init);
-    cprintf("  etext  0x%08x (virtual)\n", etext);
-    cprintf("  edata  0x%08x (virtual)\n", edata);
-    cprintf("  end    0x%08x (virtual)\n", end);
+    cprintf("  entry  0x%016lx (virtual)\n", kern_init);
+    cprintf("  etext  0x%016lx (virtual)\n", etext);
+    cprintf("  edata  0x%016lx (virtual)\n", edata);
+    cprintf("  end    0x%016lx (virtual)\n", end);
     cprintf("Kernel executable memory footprint: %dKB\n",
             (end - kern_init + 1023) / 1024);
 }
